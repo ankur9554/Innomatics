6668d16016e46b21a79f143ee9227d075549cf1a
Vikas Pandey <ankurbst95@gmail.com>
2023-03-13 10:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,36 @@
-import streamlit as st
-st.header("Hello World")
+#import flask
+from flask import Flask, request, url_for
+from markupsafe import escape
+
+
+#create app
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return 'Welcome to home page!'
+@app.route("/search")
+def search():
+    return 'Welcome to Search Page!'
+
+@app.route("/upper_case")
+def upper_case():
+    string = request.args.get('string')
+    string = str(string)
+    string = string.upper()
+    return string
+@app.route("/add")
+def sum():
+    a= int((request.args.get('a')))
+    b= int((request.args.get('b')))
+    c = a+b
+    return str(c)
+
+    
+if(__name__ == "__main__"):
+    app.run(debug=True)
+
+
+
+
+
+
